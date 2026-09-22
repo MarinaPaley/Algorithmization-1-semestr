e2a4b49d9dc3d88aed24c636c1359f3fbc58c957
MarinaPaley <marina_paley@mail.ru>
2026-09-22 15:43:24 +0300

    Начало работы

--- Task1/Task1-1/Program.cs
+++ Task1/Task1-1/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const double a = 2.0;
+            const double b = 1.0;
+            const double c = 3.0;
+
+            Console.WriteLine("Введите x = ");
+            string input = Console.ReadLine();
+
+            double x = double.Parse(input);
+            double numerator = Numerator(a, b, c, x);
+            double denominator = Denominator(c, x);
+
+            double y = numerator / denominator;
+
+           Console.WriteLine($"x = {x}, y = {y}");
+        }
+
+        static double Numerator(double a, double c, double b, double x)
+        {
+            return (a * Math.Pow(Math.Abs(x), 3) +
+                b * Math.Sqrt(x * x + 1));
+        }
+
+        static double Denominator(double c, double x)
+        {
+            return c + Math.Sin(x) * Math.Sin(x)
+                + Math.Cos(x) * Math.Cos(x);
         }
     }
 }
